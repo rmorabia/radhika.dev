5a18048e5aa19b7eacd5a16ffbd3e34d4780c1d6
Radhika Morabia <hi@rmorabia.com>
2016-04-01 20:31:11 -0700

    wait lol that did not work

---
+++ js/caramel.js
@@ -0,0 +1,45 @@
+/*!
+// Caramel (http://caramel.ga/)
+// Copyright 2015, All Rights Reserved
+// @version 1.5.0
+// @license SEE <LICENSE>
+*/
+$(document).ready(function() {
+
+    var $nav = $('.nav');
+    var $outside = $(document);
+
+    $nav.on('click', '.menu', function(e){
+        e.stopPropagation();
+        e.preventDefault();
+
+        var $this = $(this);
+        $this.parents($nav).toggleClass('open');
+
+        $outside.one('click', function(e) {
+            $nav.removeClass('open');
+        });
+    });
+
+    $nav.on('click', '.dropdownitem', function(e){
+        e.stopPropagation();
+        e.preventDefault();
+
+        var $this = $(this);
+        $this.parent('.dropdown').toggleClass('open');
+
+        $outside.one('click', function(e) {
+            $nav.removeClass('open');
+        });
+    });
+
+    // Alert Dismissables
+    $(".dismiss").click(function() {
+        $(this).closest("#note").fadeOut(500, function() {
+            $(this).remove();
+        });
+    });
+
+    //Force a resize to init.
+    $(window).resize();
+});

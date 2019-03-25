e5d15d3b0d8c57c836b6427537ea2be89dd03ff7
Radhika Morabia <radhika.morabia@l-tron.com>
2019-03-24 22:52:48 -0400

    Continuing through the official tutorial

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,33 +1,25 @@
 import React from "react"
-import Header from "../components/header"
-import Container from "../components/container"
-import styles from './about.module.css'
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
 
-const User = props => (
-  <div className={styles.user}>
-    <img src={props.avatar} className={styles.avatar} alt="" />
-    <div className={styles.description}>
-      <h2 className={styles.username}>{props.username}</h2>
-      <p className={styles.excerpt}>{props.excerpt}</p>
+export default ({data}) => (
+  <Layout>
+    <h1>{data.site.siteMetadata.title}</h1>
+    <div>
+      <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
     </div>
-  </div>
+  </Layout>
 )
 
-export default () => (
-  <Container>
-    <Header headerText="Radhika Morabia"/>
-    <Header headerText="Cool beans" />
-    <h1>About Gatsby</h1>
-    <p>Such wow. Very React.</p>
-    <User
-      username="Jane Doe"
-      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
-      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-    />
-    <User
-      username="Bob Smith"
-      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
-      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-    />
-  </Container>
-)
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default () => (
-  <div style={{ color: `teal` }}>
+  <Layout style={{ color: `teal` }}>
     <Link to="/">Home</Link>
     <Header headerText="Contact" />
     <p>Send us a message!</p>
-  </div>
+  </Layout>
 )

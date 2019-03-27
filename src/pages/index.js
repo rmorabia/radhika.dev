3bdd5d20bd795fb17ba963c713eca579b97fe660
Radhika Morabia <radhika.morabia@l-tron.com>
2019-03-27 04:10:00 -0400

    Currently not compiling

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,21 @@ export default () => (
         src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
         alt="Group of pandas eating bamboo"
       />
+      <p>
+      ```javascript
+        // In your gatsby-config.js
+        plugins: [
+          {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+              plugins: [
+                `gatsby-remark-prismjs`,
+              ]
+            }
+          }
+        ß]
+      ```
+      </p>
     </div>
   </Layout>
 )

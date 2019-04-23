109da65bdfdba1abb2a8568550d607d63cd8008c
Radhika Morabia <hi@rmorabia.com>
2019-04-23 12:10:51 -0400

    Remove onHide function
    - Move canvas.js into a component itself
    - Test to see if it works without onHide
    - Will refactor if it builds

---
+++ src/components/canvas.js
@@ -0,0 +1,16 @@
+/**
+ * demo.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2017, Codrops
+ * http://www.codrops.com
+ */
+
+import hasTouch from "../assets/three/hasTouch"
+
+class App {}
+
+export default new App()

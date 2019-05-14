f847e8d6c2007145cf5af9908c364e92e1e4c905
Radhika Morabia <hi@rmorabia.com>
2019-05-14 07:38:29 +0000

    struggling with colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,9 @@ export default () => (
     <p>I'm a Software Engineer based in Oakland, CA. I'm interested in full-stack web development & data engineering.</p>
     <p>Currently, I focus on full-stack JavaScript <i>(and all the drama that entails)</i>. I'm also learning Python. Down the line, I'm hoping to gain a good understanding of Java and CS fundamentals. <a href="http://radhika.dev/blog">Read about what I'm learning on my blog.</a> Usually updated weekly.</p>
     <p>The best way to keep up with what I'm doing is to subscribe to my newsletter below. I write about my journey in the tech industry and sprinkle in some useful advice.</p>
-    {/* <form action="https://tinyletter.com/rmorabia" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/rmorabia', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
-      <input type="text" name="email" id="tlemail" placeholder="hi@whatever.com"></input> */}
+    <form action="https://tinyletter.com/rmorabia" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/rmorabia', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
+      <input className="email" text="text" name="email" id="tlemail" placeholder="hi@whatever.com"></input>
+      <input className="submit" type="submit" value="Subscribe"></input>
     </form>
     {/* <form action="https://tinyletter.com/rmorabia" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/rmorabia', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
       <p>

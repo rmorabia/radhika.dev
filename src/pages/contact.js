9f44853988d7247ba3ffc06701e1d58eba73b274
Radhika Morabia <hi@rmorabia.com>
2019-05-23 18:40:39 -0700

    Contact Page v1

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,9 +4,14 @@ import Layout from "../components/layout"
 import Header from "../components/header"
 
 export default () => (
-  <Layout style={{ color: `teal` }}>
-    <Link to="/">Home</Link>
+  <Layout>
     <Header headerText="Contact" />
-    <p>Send us a message!</p>
+      <p>If you would like to get in touch, email me at <a href="mailto:hi@rmorabia.com">hi@rmorabia.com</a> or use the contact form below.</p>
+      <p>I am also on <a href="http://twitter.com/radhikamorabia">Twitter</a>, <a href="http://github.com/rmorabia">GitHub</a>, and <a href="http://linkedin.com/in/rmorabia">LinkedIn</a>.</p>
+      <form>
+        <input type="text" name="name" />
+        <input type="text" name="email" />
+        <input type="textarea" name="message" />
+      </form>
   </Layout>
 )

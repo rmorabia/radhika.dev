9bdc9d8895b5f6366a8c4152690d79fe846d2878
Radhika Morabia <hi@rmorabia.com>
2019-05-25 07:35:30 +0000

    Add contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,21 @@
-import React from "react"
-import { Link } from "gatsby"
-import Layout from "../components/layout"
-import Header from "../components/header"
+import React from 'react'
+import styles from './contact.module.scss'
+import Layout from '../components/layout'
+import Header from '../components/header'
 
 export default () => (
-  <Layout style={{ color: `teal` }}>
-    <Link to="/">Home</Link>
+  <Layout>
     <Header headerText="Contact" />
-    <p>Send us a message!</p>
+    <p>If you would like to get in touch, email me at <a href="mailto:hi@rmorabia.com">hi@rmorabia.com</a> or use the contact form below.</p>
+    <p>I am also on <a href="http://twitter.com/radhikamorabia">Twitter</a>, <a href="http://github.com/rmorabia">GitHub</a>, and <a href="http://linkedin.com/in/rmorabia">LinkedIn</a>.</p>
+    <form name="contact" method="POST" data-netlify="true" className={styles.contactForm}>
+      <label for="name">Name:</label>
+      <input type="text" name="name" />
+      <label for="email">Email:</label>
+      <input type="email" name="email" />
+      <label for="message">Message:</label>
+      <textarea name="message" />
+      <input type="submit" className="submit" className={styles.submit} name="Submit" />
+    </form>
   </Layout>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,18 @@
-import React from "react"
-import Layout from "../components/layout"
-import styles from "./index.module.scss"
-import { Link, StaticQuery, graphql } from 'gatsby'
+import React from 'react'
+import Layout from '../components/layout'
+import styles from './index.module.scss'
+import { Link } from 'gatsby'
 
 export default () => (
   <Layout>
     <h1 className={styles.heading}>
-      {`Hi, I'm`} <div className={styles.rainbowBackground}><span className={styles.name}>Radhika Morabia</span></div>.
+      {'Hi, I\'m'} <div className={styles.rainbowBackground}><span className={styles.name}>Radhika Morabia</span></div>.
     </h1>
     <p>I'm a Software Engineer based in Oakland, CA. I'm interested in full&#x2011;stack web development & data engineering.</p>
     <p>Currently, I work with React, Vue, and Node.js. I'm also learning Python. Down the line, I'm hoping to gain a good understanding of Java and CS fundamentals. <Link to={'/blog'}>Read about what I'm learning on my blog.</Link> Usually updated weekly.</p>
     <p>The best way to keep up with what I'm doing is to subscribe to my newsletter below. I write about my journey in the tech industry and sprinkle in some useful advice.</p>
     <form action="https://tinyletter.com/rmorabia" method="post" target="popupwindow" onsubmit="window.open('https://tinyletter.com/rmorabia', 'popupwindow', 'scrollbars=yes,width=800,height=600');return true">
-      <input className="email" text="text" name="email" id="tlemail" placeholder="hi@whatever.com"></input>
+      <input type="email" name="email" id="tlemail" placeholder="hi@whatever.com"></input>
       <input className="submit" type="submit" value="Subscribe"></input>
     </form>
     <p>If you want to keep up with a less polished version of me, I'm pretty active on Twitter at <a href="http://twitter.com/radhikamorabia">@radhikamorabia</a>.</p>

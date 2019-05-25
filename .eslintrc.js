348ef460a3a0e05ee22f180919b07af6503321bb
Radhika Morabia <hi@rmorabia.com>
2019-05-25 07:24:36 +0000

    Fix eslint issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
         "ecmaFeatures": {
             "jsx": true
         },
-        "ecmaVersion": 2018
+        "ecmaVersion": 2018,
+        "sourceType": "module"
     },
     "plugins": [
         "react"

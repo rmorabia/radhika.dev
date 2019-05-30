93e4747776ab3a83876fe7b6c60eedc8a1e9a938
Radhika Morabia <hi@rmorabia.com>
2019-05-30 03:18:34 +0000

    search! google! for! your! errors!

--- src/pages/404.js
+++ src/pages/404.js
@@ -11,24 +11,25 @@ class Lost extends Component {
       <Layout>
         <h1>Whoops! This page does not exist.</h1>
         <p>While you're here, why not take a break and play some Tetris?</p>
-        <Tetris>
-          {({
-            Gameboard,
-            points,
-            linesCleared
-          }) => {
-            // Render it however you'd like
-            return (
-              <div>
-                <Gameboard />
+        { typeof document !== 'undefined' && Tetris && 
+          <Tetris>
+            {({
+              Gameboard,
+              points,
+              linesCleared
+            }) => {
+              return (
                 <div>
-                  <p>Points: {points}</p>
-                  <p>Lines Cleared: {linesCleared}</p>
+                  <Gameboard />
+                  <div>
+                    <p>Points: {points}</p>
+                    <p>Lines Cleared: {linesCleared}</p>
+                  </div>
                 </div>
-              </div>
-            )
-          }}
-        </Tetris>
+              )
+            }}
+          </Tetris>
+        }
       </Layout>
     )
   }

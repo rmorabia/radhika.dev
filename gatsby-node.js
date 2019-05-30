f0ba2c0a62590a348c06ef881b6a59ff7c5a72b2
Radhika Morabia <hi@rmorabia.com>
2019-05-30 02:54:22 +0000

    Attempt to fix react-tetris

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,3 +40,18 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === 'build-html') {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /react-tetris/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}

ebaaef799e656cefad556b19313c3737b971f344
Radhika Morabia <hi@rmorabia.com>
2019-06-05 10:20:46 +0000

    Projects page working

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
+import Img from 'gatsby-image'
 import Layout from '../components/layout'
 import Header from '../components/header'
 
-export default () => (
+export default ({ data }) => (
   <Layout>
     <Helmet>
       <meta charSet="utf-8" />
@@ -15,9 +17,20 @@ export default () => (
     <p>To see a full directory of my code and projects, check out my <a href="http://github.com/rmorabia">GitHub</a>.</p>
     <hr />
     <h2>radhika.dev</h2>
-    {/* <Img fluid={fluid} /> */}
+    <Img fluid={data.fileName.childImageSharp.fluid} alt="" />
     <p>Portfolio website featuring canvas animation, static blogging, mobile responsiveness, accessibility, and more.</p>
     <p><b>Tools Used:</b> React, Gatsby, SCSS, and JavaScript</p>
   </Layout>
 )
 
+export const query = graphql`
+  query {
+    fileName: file(relativePath: { eq: "assets/images/radhikadev.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000, maxHeight: 1000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`

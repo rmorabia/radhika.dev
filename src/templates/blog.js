763b6cd3a7761ad979e519efbd501bfac809f788
Radhika Morabia <hi@rmorabia.com>
2021-02-21 15:20:07 -0800

    Try adding comments to the blog

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -35,6 +35,15 @@ export default ({ data }) => {
         />
         <input className='submit' type='submit' value='Subscribe' />
       </form>
+      <script
+        src='https://utteranc.es/client.js'
+        repo='rmorabia/radhika.dev'
+        issue-term='pathname'
+        label='blog post'
+        theme='github-light'
+        crossorigin='anonymous'
+        async
+      ></script>
     </Layout>
   )
 }

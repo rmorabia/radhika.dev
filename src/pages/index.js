cff7808fb49d7c0165b188fb115d79f85c78c22b
Radhika Morabia <rmorabia@opentable.com>
2021-02-23 15:10:28 -0800

    fix styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,7 @@ export default ({ data }) => {
           src='https://rmorabia.substack.com/embed'
           width='480'
           height='320'
-          style='border:1px solid #EEE; background:white;'
+          style={{ border: '1px solid #EEE', background: 'white' }}
           frameborder='0'
           scrolling='no'
         ></iframe>
